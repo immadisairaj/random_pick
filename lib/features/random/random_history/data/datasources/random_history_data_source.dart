ea573718e9ff6d27af32645eae99e6ca1b193c7d
immadisairaj <immadirajendra.sai@gmail.com>
2022-05-28 13:29:52 +0530

    add repository impl and data source for history
    - complete the data layer

---
+++ lib/features/random/random_history/data/datasources/random_history_data_source.dart
@@ -0,0 +1,99 @@
+import 'dart:convert';
+
+import 'package:hive/hive.dart';
+import 'package:random_pick/features/random/random_history/data/models/pick_history_model.dart';
+import 'package:rxdart/subjects.dart';
+
+/// data source which can be extended to implement data sources functions
+abstract class RandomHistoryDataSource {
+  /// returns all history of picked
+  ///
+  /// the main data source of the history
+  Future<Stream<List<PickHistoryModel>>> getRandomHistory();
+
+  /// add a pick to the history
+  Future<void> addRandomHistory(PickHistoryModel pickHistory);
+
+  /// get random history by pick id
+  Future<PickHistoryModel> getRandomHistoryById(String id);
+}
+
+/// implimentation of the [RandomHistoryDataSource]
+class RandomHistoryDataSourceImpl implements RandomHistoryDataSource {
+  /// constructor for [RandomHistoryDataSource] implementation
+  RandomHistoryDataSourceImpl({required Box<String> box}) : _box = box {
+    _init();
+  }
+
+  final Box<String> _box;
+
+  final _randomHistoryStreamController =
+      BehaviorSubject<List<PickHistoryModel>>.seeded(const []);
+
+  /// The key used for storing the todos locally.
+  ///
+  /// This is only exposed for testing and shouldn't be used by consumers of
+  /// this library.
+  static const kHistoryKey = '__random_history_list_key__';
+
+  void _init() {
+    final historyJson = _box.get(kHistoryKey);
+    if (historyJson != null) {
+      final historyJsonMap = (json.decode(historyJson) as List<dynamic>)
+          .cast<Map<String, dynamic>>();
+      final history = historyJsonMap
+          .map<PickHistoryModel>(
+            PickHistoryModel.fromJson,
+          )
+          .toList();
+      _randomHistoryStreamController.add(history);
+    } else {
+      _randomHistoryStreamController.add(const []);
+    }
+  }
+
+  @override
+  Future<Stream<List<PickHistoryModel>>> getRandomHistory() async =>
+      _randomHistoryStreamController.asBroadcastStream();
+
+  @override
+  Future<void> addRandomHistory(PickHistoryModel pickHistory) async {
+    final history = [..._randomHistoryStreamController.value];
+    final historyIndex = history.indexWhere((i) => i.id == pickHistory.id);
+    if (historyIndex >= 0) {
+      throw HistoryAlreadyExistsException();
+    } else {
+      history.add(pickHistory);
+    }
+
+    _randomHistoryStreamController.add(history);
+    return _box.put(
+      kHistoryKey,
+      json.encode(
+        history
+            .map<dynamic>(
+              (pickHistory) => pickHistory.toJson(),
+            )
+            .toList(),
+      ),
+    );
+  }
+
+  @override
+  Future<PickHistoryModel> getRandomHistoryById(String id) async {
+    final history = [..._randomHistoryStreamController.value];
+    final historyIndex = history.indexWhere((i) => i.id == id);
+
+    if (historyIndex >= 0) {
+      return history[historyIndex];
+    } else {
+      throw HistoryNotFoundException();
+    }
+  }
+}
+
+/// exception when history id already exists
+class HistoryAlreadyExistsException implements Exception {}
+
+/// exception when no history is found
+class HistoryNotFoundException implements Exception {}
---
+++ test/features/random/random_history/data/datasources/random_history_data_source_test.dart
@@ -0,0 +1,156 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:random_pick/features/random/random_history/data/datasources/random_history_data_source.dart';
+import 'package:random_pick/features/random/random_history/data/models/pick_history_model.dart';
+import 'package:random_pick/features/random/random_number/data/models/number_range_model.dart';
+import 'package:random_pick/features/random/random_number/data/models/random_number_picked_model.dart';
+
+class MockHiveBox extends Mock implements Box<String> {}
+
+void main() {
+  late RandomHistoryDataSourceImpl dataSource;
+  late MockHiveBox mockBox;
+
+  setUp(() {
+    mockBox = MockHiveBox();
+    dataSource = RandomHistoryDataSourceImpl(
+      box: mockBox,
+    );
+  });
+
+  test('should get data from box on init', () async {
+    // arrange
+    final tHistoryList = <PickHistoryModel>[
+      PickHistoryModel(
+        dateTime: DateTime.now(),
+        picked: RandomNumberPickedModel(
+          randomNumber: 0,
+          numberRange: NumberRangeModel(min: 0, max: 0),
+        ),
+      ),
+    ];
+    final tJson = json.encode(
+      tHistoryList
+          .map<dynamic>(
+            (pickHistory) => pickHistory.toJson(),
+          )
+          .toList(),
+    );
+    when(() => mockBox.get(RandomHistoryDataSourceImpl.kHistoryKey))
+        .thenAnswer((_) => tJson);
+    // act
+    final resultDataSource = RandomHistoryDataSourceImpl(
+      box: mockBox,
+    );
+    final result = await resultDataSource.getRandomHistory();
+    // assert
+    verify(() => mockBox.get(RandomHistoryDataSourceImpl.kHistoryKey));
+    result.listen((value) {
+      expect(value, equals(tHistoryList));
+    });
+  });
+
+  void returnVoid() {
+    return;
+  }
+
+  test('should check return value of getHistory', () async {
+    // act
+    final result = await dataSource.getRandomHistory();
+    // assert
+    verify(() => mockBox.get(RandomHistoryDataSourceImpl.kHistoryKey));
+    result.listen((value) {
+      expect(value, isA<List<PickHistoryModel>>());
+    });
+  });
+
+  test('should check add history', () async {
+    // arrange
+    final tHistoryList = <PickHistoryModel>[
+      PickHistoryModel(
+        dateTime: DateTime.now(),
+        picked: RandomNumberPickedModel(
+          randomNumber: 0,
+          numberRange: NumberRangeModel(min: 0, max: 0),
+        ),
+      ),
+    ];
+    when(() => mockBox.put(RandomHistoryDataSourceImpl.kHistoryKey, any()))
+        .thenAnswer((_) async => returnVoid());
+    // act
+    await dataSource.addRandomHistory(tHistoryList[0]);
+    final result = await dataSource.getRandomHistory();
+    // assert
+    verify(
+      () => mockBox.put(
+        RandomHistoryDataSourceImpl.kHistoryKey,
+        json.encode(
+          tHistoryList.map<dynamic>((history) => history.toJson()).toList(),
+        ),
+      ),
+    );
+    result.listen((value) {
+      expect(value, contains(tHistoryList[0]));
+    });
+  });
+
+  test('should check add history fail', () async {
+    // arrange
+    final tHistoryList = <PickHistoryModel>[
+      PickHistoryModel(
+        dateTime: DateTime.now(),
+        picked: RandomNumberPickedModel(
+          randomNumber: 0,
+          numberRange: NumberRangeModel(min: 0, max: 0),
+        ),
+      ),
+    ];
+    when(() => mockBox.put(RandomHistoryDataSourceImpl.kHistoryKey, any()))
+        .thenAnswer((_) async => returnVoid());
+    // act
+    await dataSource.addRandomHistory(tHistoryList[0]);
+    final call = dataSource.addRandomHistory;
+    // assert
+    expect(
+      () => call(tHistoryList[0]),
+      throwsA(
+        const TypeMatcher<HistoryAlreadyExistsException>(),
+      ),
+    );
+  });
+
+  test('should check get history by id', () async {
+    // arrange
+    final tHistoryList = <PickHistoryModel>[
+      PickHistoryModel(
+        dateTime: DateTime.now(),
+        picked: RandomNumberPickedModel(
+          randomNumber: 0,
+          numberRange: NumberRangeModel(min: 0, max: 0),
+        ),
+      ),
+    ];
+    when(() => mockBox.put(RandomHistoryDataSourceImpl.kHistoryKey, any()))
+        .thenAnswer((_) async => returnVoid());
+    await dataSource.addRandomHistory(tHistoryList[0]);
+    // act
+    final result = await dataSource.getRandomHistoryById(tHistoryList[0].id);
+    // assert
+    expect(result, equals(tHistoryList[0]));
+  });
+
+  test('should check get history by id failing', () async {
+    // arrange
+    final call = dataSource.getRandomHistoryById;
+    // assert
+    expect(
+      () => call('something'),
+      throwsA(
+        const TypeMatcher<HistoryNotFoundException>(),
+      ),
+    );
+  });
+}
