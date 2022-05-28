9796e4de19e91d1446cb39956f13b29cbcf20520
immadisairaj <immadirajendra.sai@gmail.com>
2022-05-28 16:19:06 +0530

    add bloc for random history

---
+++ lib/features/random/random_history/presentation/bloc/random_history_bloc.dart
@@ -0,0 +1,124 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:random_pick/core/error/failures.dart';
+import 'package:random_pick/core/usecases/usecase.dart';
+import 'package:random_pick/features/random/random_history/data/repositories/random_history_repository_impl.dart';
+import 'package:random_pick/features/random/random_history/domain/entities/pick_history.dart';
+import 'package:random_pick/features/random/random_history/domain/usecases/subscribe_random_history.dart';
+
+part 'random_history_event.dart';
+part 'random_history_state.dart';
+
+// constants to show in error state
+/// error message on add a pick history and the history already exists
+const String historyAlreadyExists =
+    'History already exists to save - please use another id';
+// const String _historyNotFoundError = 'History not found';
+
+/// business logic for random history
+class RandomHistoryBloc extends Bloc<RandomHistoryEvent, RandomHistoryState> {
+  /// Random history bloc which handles the subscription of history
+  RandomHistoryBloc({
+    required this.subscribeRandomHistory,
+  }) : super(const RandomHistoryState()) {
+    on<HistorySubscriptionRequested>(_onHistorySubscriptionRequested);
+    on<HistoryAddRequested>(_onHistoryAddRequested);
+    // on<GetHistoryByIdRequested>(_getHistoryByIdRequested);
+  }
+
+  /// usecase to subscribe to history
+  final SubscribeRandomHistory subscribeRandomHistory;
+
+  /// logic of what to do when the [HistorySubscriptionRequested] event is
+  /// dispatched
+  Future<void> _onHistorySubscriptionRequested(
+    HistorySubscriptionRequested event,
+    Emitter<RandomHistoryState> emit,
+  ) async {
+    emit(state.copyWith(status: () => RandomHistoryStatus.loading));
+
+    final failureOrResult = await subscribeRandomHistory(NoParams());
+    await failureOrResult.fold(
+      (failure) async => emit(
+        state.copyWith(
+          status: () => RandomHistoryStatus.error,
+          errorMessage: () => _mapFailureToMessage(failure),
+        ),
+      ),
+      (history) async => {
+        await emit.forEach<List<PickHistory>>(
+          history,
+          onData: (historyList) => state.copyWith(
+            status: () => RandomHistoryStatus.loaded,
+            historyList: () => historyList,
+          ),
+          onError: (_, __) => state.copyWith(
+            status: () => RandomHistoryStatus.error,
+            errorMessage: () => _mapFailureToMessage(const UnknownFailure()),
+          ),
+        )
+      },
+    );
+  }
+
+  /// logic of what to do when the [HistoryAddRequested] event is dispatched
+  FutureOr<void> _onHistoryAddRequested(
+    HistoryAddRequested event,
+    Emitter<RandomHistoryState> emit,
+  ) async {
+    final failureOrResult = await subscribeRandomHistory.putRandomHistory(
+      HistoryParams(
+        pickHistory: event.pickHistory,
+      ),
+    );
+    await failureOrResult.fold(
+      (failure) async => emit(
+        state.copyWith(
+          status: () => RandomHistoryStatus.error,
+          errorMessage: () => _mapFailureToMessage(failure),
+        ),
+      ),
+      (right) {
+        // do nothing
+      },
+    );
+  }
+
+  // implement it later when needed
+  // /// logic of what to do when the [GetHistoryByIdRequested] event is dispatched
+  // FutureOr<void> _getHistoryByIdRequested(
+  //   GetHistoryByIdRequested event,
+  //   Emitter<RandomHistoryState> emit,
+  // ) async {
+  //   final failureOrResult = await subscribeRandomHistory.getRandomHistoryById(
+  //     IdParams(
+  //       id: event.id,
+  //     ),
+  //   );
+  //   await failureOrResult.fold(
+  //     (failure) async => emit(
+  //       state.copyWith(
+  //         status: () => RandomHistoryStatus.error,
+  //         errorMessage: () => _mapFailureToMessage(failure),
+  //       ),
+  //     ),
+  //     (right) {
+  //       // do nothing
+  //     },
+  //   );
+  // }
+
+  /// maps the failure to a message using the failure type
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case HistoryAlreadyExistsFailure:
+        return historyAlreadyExists;
+      // case HistoryNotFoundFailure:
+      //   return _historyNotFoundError;
+      default:
+        return 'Unexpected error';
+    }
+  }
+}
---
+++ lib/features/random/random_history/presentation/bloc/random_history_event.dart
@@ -0,0 +1,41 @@
+part of 'random_history_bloc.dart';
+
+/// abstract class event for the random history bloc
+abstract class RandomHistoryEvent extends Equatable {
+  /// creates a random history event
+  const RandomHistoryEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+/// dispatch this event to subscribe to the history
+class HistorySubscriptionRequested extends RandomHistoryEvent {
+  /// subscribe to the history
+  const HistorySubscriptionRequested();
+}
+
+/// dispatch this event to add a pick in the history
+class HistoryAddRequested extends RandomHistoryEvent {
+  /// create a new [HistoryAddRequested] with the [pickHistory] to be added
+  const HistoryAddRequested({required this.pickHistory});
+
+  /// the pick history to be added
+  final PickHistory pickHistory;
+
+  @override
+  List<Object> get props => [pickHistory];
+}
+
+// /// dispatch this event to get a pick in the history by [id]
+// class GetHistoryByIdRequested extends RandomHistoryEvent {
+//   /// create a new [GetHistoryByIdRequested] with the [id]
+//   /// to get the pick history
+//   const GetHistoryByIdRequested({required this.id});
+
+//   /// the id of the pick history to be retrieved
+//   final String id;
+
+//   @override
+//   List<Object> get props => [id];
+// }
