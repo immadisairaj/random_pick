40cbaf02324e4e894c7258bb68da654c2ab75377
immadisairaj <immadirajendra.sai@gmail.com>
2022-05-28 20:23:21 +0530

    fix random list repository methods
    - revert few changes from previous commit

--- lib/features/random/random_list/data/repositories/random_list_repository_impl.dart
+++ lib/features/random/random_list/data/repositories/random_list_repository_impl.dart
@@ -31,7 +31,7 @@ class RandomListRepositoryImpl implements RandomListRepository {
 
   @override
   Future<Either<Failure, void>> addItemToPool(Item item) async {
-    return Right(await dataSource.addItemToPool(item as ItemModel));
+    return Right(await dataSource.addItemToPool(_convertItemToModel(item)));
   }
 
   @override
@@ -49,7 +49,7 @@ class RandomListRepositoryImpl implements RandomListRepository {
   Future<Either<Failure, void>> removeItemFromPool(Item item) async {
     try {
       return Right(
-        await dataSource.removeItemFromPool(item as ItemModel),
+        await dataSource.removeItemFromPool(_convertItemToModel(item)),
       );
     } on ItemNotFoundException {
       return Left(ItemNotFoundFailure());
@@ -59,9 +59,19 @@ class RandomListRepositoryImpl implements RandomListRepository {
   @override
   Future<Either<Failure, void>> updateItemPool(List<Item> items) async {
     return Right(
-      await dataSource.updateItemPool(items as List<ItemModel>),
+      await dataSource.updateItemPool(
+        items.map(_convertItemToModel).toList(),
+      ),
     );
   }
+}
+
+ItemModel _convertItemToModel(Item item) {
+  return ItemModel(
+    id: item.id,
+    text: item.text,
+    selected: item.selected,
+  );
 }
 
 /// failure when the length of the item pool is 0 or too long
